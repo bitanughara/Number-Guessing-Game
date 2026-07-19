726eca63cf2743f03442a55ddde6cd12e04a25d8
Bitanu Ghara <bitanughara18@gmail.com>
2026-07-20 00:01:34 +0530

    Add files via upload

---
+++ Number_Guessing_Game.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Number_Guessing_Game{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int again=1;
+        System.out.println("===== Number Guessing Game Start =====\n");
+        while(again==1){
+            int number=67;
+            int guess;
+            int attempts=0;
+            while(true){
+                System.out.print("Enter your guess :");
+                guess = sc.nextInt();
+                attempts++;
+                if(guess==number){
+                    System.out.println("Correct ! You guessed the number.");
+                    System.out.println("Total attempts :"+attempts);
+                    System.out.println("\n===== Number Guessing Game End =====");
+                    System.out.print("\nEnter 1 to Play Again or 0 to Exit: ");
+                    again = sc.nextInt();
+                    break;
+                }
+                else if(guess<number){
+                    System.out.println("Guess a High Number!");
+                }
+                else if(guess>number){
+                    System.out.println("Guess a Low Number!");
+                }
+            }
+        }
+        System.out.println("\nThank You for playing !");
+        sc.close();
+        
+    }
+}
